e16d93436b525036530653eafd26b0824429eabf
Abdulaziz Yakubov <abdulazizyakubov@gmail.com>
2018-04-22 21:53:41 +0500

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -35,11 +35,12 @@ socket.on('newLocationMessage', function(message){
 
 jQuery('#message-form').on('submit', function(e){
   e.preventDefault();
+  var messageTextbox = jQuery('[name=message]');
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
-  }, function(data){
-    console.log(data);
+    text: messageTextbox.val()
+  }, function(){
+    messageTextbox.val('');
   });
 });
 
@@ -49,12 +50,16 @@ locationButton.on('click', function(){
     return alert('Geolocation not supported by your browser');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function(position){
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('create-location', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     })
   }, function(){
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location');
   })
 });
--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('This is from server');
+    callback();
    });
 
 socket.on('create-location', (coords) => {
